4d2cb45f151184697810a6e795debc720003c483
Ramon Gebben <ramon@ra-ge.net>
2015-02-02 22:41:10 +0100

    fixing permissions

--- fish/after_install.fish
+++ fish/after_install.fish
@@ -3,9 +3,9 @@ function after_install
   git pull origin master
 
   rvm install ruby-2.1.0
-
   install-nginx
-
+  fix-permissions
+  
   crow notice "You are ready to setup the first user"
   crow notice "Use: add_user to add a user"
 
