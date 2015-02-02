1db14442c7ec06b513ebd193da2406b7f61f957e
Ramon Gebben <ramon@ra-ge.net>
2015-02-02 16:26:38 +0100

    Update install script

--- install/install.sh
+++ install/install.sh
@@ -8,7 +8,7 @@ echo "LC_BYOBU=1" >> /etc/default/locale  # make byoby system-wide default, if i
 locale-gen en_US en_US.UTF-8
 dpkg-reconfigure locales
 mkdir /root/.byobu
-echo "source /devstar/config/byobu" > /root/.byobu/.tmux.conf
+echo "source /serenity/config/byobu" > /root/.byobu/.tmux.conf
 
 # upgrade to zero-day
 apt-get update
@@ -16,38 +16,38 @@ apt-get -y upgrade
 
 # add support for ppa
 apt-get install -y python-software-properties
+curl -sL https://deb.nodesource.com/setup | sudo bash -
 
 # repositories
-add-apt-repository -y ppa:keithw/mosh            # mosh
-apt-add-repository -y ppa:fish-shell/release-2   # fish
+add-apt-repository -y ppa:keithw/mosh  # mosh
 apt-get update
 
 # install it all at once
-apt-get -y install mosh byobu fish git toilet fail2ban
+apt-get -y install byobu fish git toilet fail2ban nodejs
 
 # set default group
-addgroup devstar
-usermod -g devstar root
-echo "%devstar   ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/devstar
-chmod 0440 /etc/sudoers.d/devstar
+addgroup serenity
+usermod -g serenity root
+echo "%serenity   ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/serenity
+chmod 0440 /etc/sudoers.d/serenity
 
-# checkout devstar
-mkdir -p /devstar
-cd /devstar
-git clone https://github.com/RamonGebben/devstar.git .
-mkdir -p /devstar/sites
-mkdir -p /devstar/flags
+# checkout serenity
+mkdir -p /serenity
+cd /serenity
+git clone https://github.com/RamonGebben/serenity.git .
+mkdir -p /serenity/sites
+mkdir -p /serenity/flags
 
 # fix ownership
-chown -R root:devstar /devstar 
-chown -R root:devstar /devstar
+chown -R root:serenity /serenity 
+chown -R root:serenity /serenity
 
 # link fish functions
-ln -s /devstar/fish /etc/fish/functions
+ln -s /serenity/fish /etc/fish/functions
 
 # fix default umask
 sed -i 's/UMASK\s*022/UMASK 002/g' /etc/login.defs
 
-echo "***************************"
+echo "==========================================================================="
 echo "REBOOT and run ds-update"
 #reboot
