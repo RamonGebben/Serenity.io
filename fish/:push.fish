2212eef22e402fb82ec918b115be61f44628068c
Ramon Gebben <ramon@ra-ge.net>
2015-02-02 21:05:16 +0100

    updated some fish scripts

--- fish/:push.fish
+++ fish/:push.fish
@@ -1,10 +1,8 @@
 function :push -a branch
    switch "$branch"
     case ""
- 	say "uuf push it"; say "push it real good"
 	git push origin master
     case '*'
-    	 say "uuf push it"; say "push it real good"
     	 git push origin $branch
     end
 end
